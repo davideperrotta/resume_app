7aaf662fecf8c0f2b361b5693acf50ef6cb615ac
dperrotta <dperrotta>
2024-04-20 00:04:30 +0200

    organize columns to prepare mobile layout

--- lib/main.dart
+++ lib/main.dart
@@ -23,20 +23,132 @@ class MyApp extends StatelessWidget {
   }
 }
 
+final containerWidth = 500.0;
+
+TextStyle titleStyle = TextStyle(
+  fontSize: 18.0,
+  fontWeight: FontWeight.bold,
+  color: Colors.blue,
+  letterSpacing: 3,
+  height: 3,
+);
+
+TextStyle descriptionStyle = TextStyle(
+    fontSize: 16.0, fontWeight: FontWeight.normal, color: Colors.black);
+
+class LeftColumn extends StatelessWidget {
+
+  @override
+  Widget build (BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          'PERSONAL EXPERIENCES',
+          style: titleStyle,
+        ),
+        Container(
+          width: containerWidth,
+          child: Text(
+            'I’m a qualified ICT Engineer with Engineering Master’s Degree. I have relevant experience as Technical Leader and Developer, working as Frontend, Backend and Mobile Developer, using Cloud solutions. My functional roles are coordination among company management, customers management and development team to target the customers needs. I love coding and working with high skilled teams. I was born in 1991 in Catanzaro (Italy), and I work in digital industry from 2014, started at University. \n\nMy hobbies are coding, playing bass guitar and electric guitar in a band, doing some sports like running, mountain biking, kayaking, and reading books.',
+            style: descriptionStyle,
+          ),
+        ),
+        Text(
+          'SKILLS',
+          style: titleStyle,
+        ),
+        Container(
+          width: containerWidth,
+          child: Text(
+            '- Software Development\n' +
+                '- Architecture Design\n' +
+                '- Database Development\n' +
+                '- Technical management\n' +
+                '- Training\n',
+            style: descriptionStyle,
+          ),
+        ),
+        Text(
+          'RELEVANT EXPERIENCES',
+          style: titleStyle,
+        ),
+        Container(
+          width: containerWidth,
+          child: Text(
+            '- Development of frontend, backend, mobile applications\n' +
+                '- Design and development of relational and non-relational databases (SQL and NoSQL)\n' +
+                '- Performance analysis of application loads, development of caching solutions to improve performance\n' +
+                '- Development of solutions for securing applications (Penetration Testing)\n' +
+                '- Design and development of Cloud infrastructure solutions on AWS\n' +
+                '- Coordination of the development team and trainer for junior and middle resources of the team\n' +
+                '- Analysis of customer requirements, drafting of technical solutions and cost estimates\n',
+            style: descriptionStyle,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class RightColumn extends StatelessWidget {
+
+  @override
+  Widget build (BuildContext context) {
+    return Column (
+      children: [
+        Text(
+          'PROFESSIONAL EXPERIENCES',
+          style: titleStyle,
+        ),
+        Container(
+          width: containerWidth,
+          child: Text(
+            'Technical Leader, Full Stack Developer\nDeloitte Digital - (July 2018 - now) \nTechnical leader, lead developer for frontend, backend and mobile projects. Cloud architectures designer \nMain skills: JavaScript, TypeScript, React, React Native, Sass, NextJS, NestJS, Flutter, NodeJS, Redis, WebSocket, MongoDB, AWS Cloud, Docker, CI/CD, Python\n',
+            style: descriptionStyle,
+          ),
+        ),
+        Container(
+          width: containerWidth,
+          child: Text(
+            'Frontend Developer \nThinkOpen SPA - (October 2017 - July 2018) \nDeveloper for public administrations and banking platforms. Frontend developer \nMain skills: JavaScript, Sass, jQuery, Java, React\n',
+            style: descriptionStyle,
+          ),
+        ),
+        Container(
+          width: containerWidth,
+          child: Text(
+            'Full Stack Developer \nFreelance for web agencies - (March 2015 - April 2017) \nBackend and Frontend developer for e-commerce platforms. \nMain skills: PHP, Apache, Linux, MySQL, WordPress and others PHP based CMS\n',
+            style: descriptionStyle,
+          ),
+        ),
+        Text(
+          'TRAINING',
+          style: titleStyle,
+        ),
+        Container(
+          width: containerWidth,
+          child: Text(
+            'Politecnico di Milano, 2023 \nState exam for qualification of ICT Engineer \n\nUniversità Magna Graecia di Catanzaro, 2017 \nMaster’s Degree in Biomedical Engineering, 106/110 \n\nUniversità Magna Graecia di Catanzaro, 2015 \nBachelor Degree in Computer and Biomedical Engineering, 86/110 \n\nScientific High School Luigi Siciliani, Catanzaro, 2005 - 2010\n',
+            style: descriptionStyle,
+          ),
+        ),
+        Text(
+          'CERTIFICATIONS',
+          style: titleStyle,
+        ),
+        Container(
+          width: containerWidth,
+          child: Text(
+            '- AWS Cloud Practitioner \n- Adobe Experience Manager Sites Developer \n- Salesforce AI Associate \n- Business English Level 10/10 by Learnship\n',
+            style: descriptionStyle,
+          ),
+        ),
+      ]
+    );
+  }
+}
+
 class CustomComponent extends StatelessWidget {
-  final containerWidth = 500.0;
-
-  TextStyle titleStyle = TextStyle(
-    fontSize: 18.0,
-    fontWeight: FontWeight.bold,
-    color: Colors.blue,
-    letterSpacing: 3,
-    height: 3,
-  );
-
-  TextStyle descriptionStyle = TextStyle(
-      fontSize: 16.0, fontWeight: FontWeight.normal, color: Colors.black);
-
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -109,100 +221,12 @@ class CustomComponent extends StatelessWidget {
               children: [
             Expanded(
                 child: Column(children: [
-              Text(
-                'PERSONAL EXPERIENCES',
-                style: titleStyle,
-              ),
-              Container(
-                width: containerWidth,
-                child: Text(
-                  'I’m a qualified ICT Engineer with Engineering Master’s Degree. I have relevant experience as Technical Leader and Developer, working as Frontend, Backend and Mobile Developer, using Cloud solutions. My functional roles are coordination among company management, customers management and development team to target the customers needs. I love coding and working with high skilled teams. I was born in 1991 in Catanzaro (Italy), and I work in digital industry from 2014, started at University. \n\nMy hobbies are coding, playing bass guitar and electric guitar in a band, doing some sports like running, mountain biking, kayaking, and reading books.',
-                  style: descriptionStyle,
-                ),
-              ),
-              Text(
-                'SKILLS',
-                style: titleStyle,
-              ),
-              Container(
-                width: containerWidth,
-                child: Text(
-                  '- Software Development\n' +
-                      '- Architecture Design\n' +
-                      '- Database Development\n' +
-                      '- Technical management\n' +
-                      '- Training\n',
-                  style: descriptionStyle,
-                ),
-              ),
-              Text(
-                'RELEVANT EXPERIENCES',
-                style: titleStyle,
-              ),
-              Container(
-                width: containerWidth,
-                child: Text(
-                  '- Development of frontend, backend, mobile applications\n' +
-                      '- Design and development of relational and non-relational databases (SQL and NoSQL)\n' +
-                      '- Performance analysis of application loads, development of caching solutions to improve performance\n' +
-                      '- Development of solutions for securing applications (Penetration Testing)\n' +
-                      '- Design and development of Cloud infrastructure solutions on AWS\n' +
-                      '- Coordination of the development team and trainer for junior and middle resources of the team\n' +
-                      '- Analysis of customer requirements, drafting of technical solutions and cost estimates\n',
-                  style: descriptionStyle,
-                ),
-              ),
+                LeftColumn(),
             ])),
             Expanded(
                 child: Column(
               children: [
-                Text(
-                  'PROFESSIONAL EXPERIENCES',
-                  style: titleStyle,
-                ),
-                Container(
-                  width: containerWidth,
-                  child: Text(
-                    'Technical Leader, Full Stack Developer\nDeloitte Digital - (July 2018 - now) \nTechnical leader, lead developer for frontend, backend and mobile projects. Cloud architectures designer \nMain skills: JavaScript, TypeScript, React, React Native, Sass, NextJS, NestJS, Flutter, NodeJS, Redis, WebSocket, MongoDB, AWS Cloud, Docker, CI/CD, Python\n',
-                    style: descriptionStyle,
-                  ),
-                ),
-                Container(
-                  width: containerWidth,
-                  child: Text(
-                    'Frontend Developer \nThinkOpen SPA - (October 2017 - July 2018) \nDeveloper for public administrations and banking platforms. Frontend developer \nMain skills: JavaScript, Sass, jQuery, Java, React\n',
-                    style: descriptionStyle,
-                  ),
-                ),
-                Container(
-                  width: containerWidth,
-                  child: Text(
-                    'Full Stack Developer \nFreelance for web agencies - (March 2015 - April 2017) \nBackend and Frontend developer for e-commerce platforms. \nMain skills: PHP, Apache, Linux, MySQL, WordPress and others PHP based CMS\n',
-                    style: descriptionStyle,
-                  ),
-                ),
-                Text(
-                  'TRAINING',
-                  style: titleStyle,
-                ),
-                Container(
-                  width: containerWidth,
-                  child: Text(
-                    'Politecnico di Milano, 2023 \nState exam for qualification of ICT Engineer \n\nUniversità Magna Graecia di Catanzaro, 2017 \nMaster’s Degree in Biomedical Engineering, 106/110 \n\nUniversità Magna Graecia di Catanzaro, 2015 \nBachelor Degree in Computer and Biomedical Engineering, 86/110 \n\nScientific High School Luigi Siciliani, Catanzaro, 2005 - 2010\n',
-                    style: descriptionStyle,
-                  ),
-                ),
-                Text(
-                  'CERTIFICATIONS',
-                  style: titleStyle,
-                ),
-                Container(
-                  width: containerWidth,
-                  child: Text(
-                    '- AWS Cloud Practitioner \n- Adobe Experience Manager Sites Developer \n- Salesforce AI Associate \n- Business English Level 10/10 by Learnship\n',
-                    style: descriptionStyle,
-                  ),
-                ),
+                RightColumn(),
               ],
             ))
           ])
