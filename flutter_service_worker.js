d510ab59c2b575aa4443e8b9dc07e00cebc4a774
dperrotta <davide.perrotta@consulenti.mediolanum.it>
2024-05-05 15:48:55 +0200

    Built web
    Branch: main
    Commit: 5ae6245b0cf9a95f1d26213430ec829e6d62c229
    
    package:peanut 5.0.0

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "6d3ae603951301f156f1393e173ea420",
-"index.html": "3d06a6a72020173bd5e3edc29cacba53",
-"/": "3d06a6a72020173bd5e3edc29cacba53",
-"main.dart.js": "913884e7b188ea035c1b1364f3c20ddc",
+"index.html": "6a958f5207238d1806bedc7d8bfca0d2",
+"/": "6a958f5207238d1806bedc7d8bfca0d2",
+"main.dart.js": "cc79886c34013ab3ec8931002b1183fa",
 "flutter.js": "c71a09214cb6f5f8996a531350400a9a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
